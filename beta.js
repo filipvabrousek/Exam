a36bede23e07509523095a4a79f07ceace8d2b9b
Filip Vabrousek <fvabrousek@yahoo.com>
2016-08-16 21:29:31 +0200

    Update beta.js
    Added render engine

--- beta.js
+++ beta.js
@@ -1,4 +1,6 @@
-function B(e){var t={};return t.selector=e,t.element=document.querySelector(t.selector),t.show=function(){t.element.style.display="block"},t.hide=function(){t.element.style.display="none"},t.addClass=function(e){t.element.classList.add(e)},t.removeClass=function(e){t.element.classList.remove(e)},t.height=function(){return t.element.offsetHeight},t.width=function(){return t.element.offsetWidth},t.text=function(){return t.element.innerText},t.html=function(){return t.element.innerHTML},t.val=function(){return t.element.value},t.attr=function(e,n){return n?(t.element.setAttribute(e,n),t):t.element.getAttribute(e)},t.on=function(e,n){return t.element["on"+e]=n,t},t.RENDER=function(e){t.element.innerHTML+=e},t}
+function B(c){var a={};a.selector=c;a.element=document.querySelector(a.selector);a.show=function(){a.element.style.display="block"};a.hide=function(){a.element.style.display="none"};a.addClass=function(b){a.element.classList.add(b)};a.removeClass=function(b){a.element.classList.remove(b)};a.height=function(){return a.element.offsetHeight};a.width=function(){return a.element.offsetWidth};a.text=function(){return a.element.innerText};a.html=function(){return a.element.innerHTML};a.val=function(){return a.element.value};
+a.attr=function(b,c){if(!c)return a.element.getAttribute(b);a.element.setAttribute(b,c);return a};a.on=function(b,c){a.element["on"+b]=c;return a};a.RENDER=function(b){a.element.innerHTML+=b};return a}
+var BRender=function(c,a){for(var b=/<%([^%>]+)?%>/g,h=/(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,e="var r=[];\n",f=0,d,g=function(a,b){b?e+=a.match(h)?a+"\n":"r.push("+a+");\n":e+=""!=a?'r.push("'+a.replace(/"/g,'\\"')+'");\n':"";return g};d=b.exec(c);)g(c.slice(f,d.index))(d[1],!0),f=d.index+d[0].length;g(c.substr(f,c.length-f));e+='return r.join("");';return(new Function(e.replace(/[\r\t\n]/g,""))).apply(a)},template="<h1>Fruits</h1><%if(this.show) {%><%for(var index in this.fruits) {%><h2><%this.fruits[index]%></h2><%}%><%} else {%><p>Content of the template is hidden, set <b>show</b> property to <b>true</b></p><%}%>";
 /*
 show()
 hide()
@@ -94,6 +96,50 @@ self.val = function(){
     return self;
 
   }
+  
+  
+  //RENDER ENGINE
+  var BRender = function(html, options) {
+  var re = /<%([^%>]+)?%>/g,
+    reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
+    code = 'var r=[];\n',
+    cursor = 0,
+    match;
+  
+  //ADD FUNCTION---------------------------------------------------
+  var add = function(line, js) {
+    js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+      (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+    return add;
+  }
+  
+  while (match = re.exec(html)) {
+    add(html.slice(cursor, match.index))(match[1], true);
+    cursor = match.index + match[0].length;
+  }
+  
+  //ADD THE CODE add() FUNCTION IS CALLED------------------------------------------
+  add(html.substr(cursor, html.length - cursor));
+  code += 'return r.join("");';
+  return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+}
+
+//DEFINE TEMPLATE------------------------------------------------
+var template =
+  '<h1>Fruits</h1>' +
+  '<%if(this.show) {%>' +
+  '<%for(var index in this.fruits) {%>' +
+  '<h2><%this.fruits[index]%></h2>' +
+  '<%}%>' +
+  '<%} else {%>' +
+  '<p>Content of the template is hidden, set <b>show</b> property to <b>true</b></p>' +
+  '<%}%>';
+
+//SHOW OUTPUT----------------------------------------------------
+document.querySelector(".demo").innerHTML = BRender(template, {
+  fruits: ["Orange", "Mango", "Pineapple"],
+  show: true
+});
 
 
 

e258ced337f549616184a4b552ec4b66aec6ba93
Filip Vabrousek <fvabrousek@yahoo.com>
2017-01-01 08:12:00 +0100

    Update Ello.js

--- Ello.js
+++ Ello.js
@@ -217,3 +217,102 @@ button {
 }
 26.12.2016 - 17:58
 */
+
+
+
+
+
+
+
+
+
+
+
+
+/*ELLO DOM MANIPULATING LIBRARY UNCOMENT AND COPY TO A NEW FILE!
+
+function E(selector) {
+  var self = {};
+  self.selector = selector;
+  self.element = document.querySelector(self.selector);
+  //HIDE and SHOW functions
+  self.show = function() {
+    self.element.style.display = "block";
+  }
+  self.hide = function() {
+      self.element.style.display = "none";
+    }
+    //ADDCLASS and REMOVECLASS
+  self.addClass = function(className) {
+    self.element.classList.add(className);
+  }
+  self.removeClass = function(className) {
+      self.element.classList.remove(className)
+    }
+    //WIDTH and HEIGHT functions
+  self.height = function() {
+    return self.element.offsetHeight;
+  }
+
+  self.width = function() {
+      return self.element.offsetWidth;
+    }
+    //text function - RETURNS!! ONLY text value
+  self.text = function() {
+      return self.element.innerText;
+    }
+    //html function - RETURNS!! html value
+  self.html = function() {
+    return self.element.innerHTML;
+  }
+  self.val = function() {
+      return self.element.value;
+    }
+    //ATTR function
+  self.attr = function(name, value) {
+      if (!value) return self.element.getAttribute(name)
+      self.element.setAttribute(name, value);
+      return self;
+    }
+    //ON function
+  self.on = function(type, callback) {
+    self.element["on" + type] = callback;
+    return self;
+  }
+
+  //RENDER function
+  self.add2HTML = function(html) {
+    self.element.innerHTML += html;
+
+  }
+  return self;
+};
+
+
+
+
+
+
+
+*/
+
+/*
+ELLO DOM EXAMPLE:
+
+HTML:
+<input placeholder="Enter a number" type="number"></input>
+<button>Show square root</button>
+
+
+CSS:
+var btn = E("button");
+btn.on("click", function() {
+var inp = E("input").val();
+console.log(Math.sqrt(inp));
+});
+
+
+
+
+
+*/

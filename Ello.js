744aa51b161d834781580d288edd33af12d9e77d
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-01 19:03:44 +0100

    Updated to ES6
    Both libraries updated to ES6

--- Ello.js
+++ Ello.js
@@ -220,64 +220,57 @@ button {
 /*ELLO DOM MANIPULATING LIBRARY UNCOMENT AND COPY TO A NEW FILE!
 
 function E(selector) {
-  var self = {};
+  const self = {};
   self.selector = selector;
   self.element = document.querySelector(self.selector);
   //HIDE and SHOW functions
-  self.show = function() {
+  self.show = () => {
     self.element.style.display = "block";
   }
-  self.hide = function() {
+  self.hide = () => {
       self.element.style.display = "none";
     }
     //ADDCLASS and REMOVECLASS
-  self.addClass = function(className) {
+  self.addClass = className => {
     self.element.classList.add(className);
   }
-  self.removeClass = function(className) {
+  self.removeClass = className => {
       self.element.classList.remove(className)
     }
     //WIDTH and HEIGHT functions
-  self.height = function() {
-    return self.element.offsetHeight;
-  }
+  self.height = () => self.element.offsetHeight
 
-  self.width = function() {
-      return self.element.offsetWidth;
-    }
+  self.width = () => self.element.offsetWidth
     //text function - RETURNS!! ONLY text value
-  self.text = function() {
-      return self.element.innerText;
-    }
+  self.text = () => self.element.innerText
     //html function - RETURNS!! html value
-  self.html = function() {
-    return self.element.innerHTML;
-  }
-  self.val = function() {
-      return self.element.value;
-    }
+  self.html = () => self.element.innerHTML
+  self.val = () => self.element.value
     //ATTR function
-  self.attr = function(name, value) {
+  self.attr = (name, value) => {
       if (!value) return self.element.getAttribute(name)
       self.element.setAttribute(name, value);
       return self;
     }
     //ON function
-  self.on = function(type, callback) {
-    self.element["on" + type] = callback;
+  self.on = (type, callback) => {
+    self.element[`on${type}`] = callback;
     return self;
   }
 
   //RENDER function
-  self.add2HTML = function(html) {
+  self.add2HTML = html => {
     self.element.innerHTML += html;
 
   }
   return self;
 };
 
-
-
+const name = "your-name-here";
+const btn = E("button");
+btn.on("click", () => {
+  console.log("Hello " + name + "! ");
+});
 
 
 
@@ -286,21 +279,5 @@ function E(selector) {
 
 /*
 ELLO DOM EXAMPLE:
-
-HTML:
-<input placeholder="Enter a number" type="number"></input>
-<button>Show square root</button>
-
-
-CSS:
-var btn = E("button");
-btn.on("click", function() {
-var inp = E("input").val();
-console.log(Math.sqrt(inp));
-});
-
-
-
-
-
+<button>Log greeting into console</button>
 */

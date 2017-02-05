8fd12c74881b7114684f3042ee11f4251a9f6f71
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-05 09:02:46 +0100

    Update 9.3-Promises-generators-modules.js

--- 9.3-Promises-generators-modules.js
+++ 9.3-Promises-generators-modules.js
@@ -9,7 +9,23 @@ const m = timeout(5000).then(() => {
     console.log("done!");
 });
 
+/*--------------------ITERATORS-------------------*/
+    function makeIterator(array){
+    var nextIndex = 0;
     
+    return {
+       next: function(){
+           return nextIndex < array.length ?
+               {value: array[nextIndex++], done: false} :
+               {done: true};
+       }
+    }
+}
+
+var it = makeIterator(['yo', 'ya']);
+console.log(it.next().value); // 'yo'
+console.log(it.next().value); // 'ya'
+console.log(it.next().done);  // true
 
     
 /*--------------------GENERATORS-------------------*/
@@ -26,3 +42,28 @@ console.log(gen.next());
 console.log(gen.next());
 console.log(gen.next());
     
+
+/*--------------------MODULES-------------------*/
+
+//utility.js
+function generateRandom() {
+    return Math.random();
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+export { generateRandom, sum }
+
+//app.js
+import { generateRandom, sum } from 'utility';
+
+console.log(generateRandom()); //logs a random number
+console.log(sum(1, 2)); //3
+
+
+// var utils = {}   export default utils;
+
+
+

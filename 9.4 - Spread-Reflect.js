9245cb4ff395fbca065afdf04e8e63d451db5123
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-08 22:33:18 +0100

    Update 9.4 - Spread-Reflect.js

--- 9.4 - Spread-Reflect.js
+++ 9.4 - Spread-Reflect.js
@@ -12,6 +12,24 @@ function sum(... numbers) {
 console.log(sum(1)); // 1
 console.log(sum(1, 2, 3, 4, 5)); // 15
 
+   
+/*---------------------------------NEXT EXAMPLES-------------------------------*/
+var a = [2, 3, 4];
+var b = [1, ...a, 5];
+console.log(b); //1, 2, 3, 4, 5
+
+function foo(x, y, ...z) {
+    console.log(x, y, z);
+}
+
+foo(1, 2, 3, 4, 5); // 1 2 [3,4,5]
+
+
+function foo(...args) {
+
+    args.shift();
+    console.log(...args);
+}
 
 /*-----------------------REFLECT----------------------*/
 

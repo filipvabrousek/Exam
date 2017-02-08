bfcee9b1df49cf989f1ccf1c4ff3f0b70da9ccf9
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-08 22:30:21 +0100

    Update 9.3-Promises-generators-iterators-modules.js

--- 9.3-Promises-generators-iterators-modules.js
+++ 9.3-Promises-generators-iterators-modules.js
@@ -10,22 +10,19 @@ const m = timeout(5000).then(() => {
 });
 
 /*--------------------ITERATORS-------------------*/
-    function makeIterator(array){
-    var nextIndex = 0;
-    
-    return {
-       next: function(){
-           return nextIndex < array.length ?
-               {value: array[nextIndex++], done: false} :
-               {done: true};
-       }
-    }
-}
+var arr = [4, 5, 6, 7, 8, 9];
 
-var it = makeIterator(['yo', 'ya']);
-console.log(it.next().value); // 'yo'
-console.log(it.next().value); // 'ya'
-console.log(it.next().done);  // true
+arr[Symbol.iterator] = function*() {
+    var idx = 1;
+    do {
+        yield this[idx];
+    } while ((idx += 2) < this.length);
+};
+
+for (var v of arr) {
+    console.log(v);
+}
+// 5 7 9 (just values from odd indexes)
 
     
 /*--------------------GENERATORS-------------------*/

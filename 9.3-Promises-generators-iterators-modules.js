9649f2197a18bbc740e990cda5b9414618dfa83e
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-15 16:30:57 +0100

    Update 9.3-Promises-generators-iterators-modules.js

--- 9.3-Promises-generators-iterators-modules.js
+++ 9.3-Promises-generators-iterators-modules.js
@@ -1,4 +1,4 @@
-/*--------------------PROMISES-------------------*/
+/*------------------------PROMISES--------------1----------*/
 function timeout(duration) {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, duration);
@@ -8,6 +8,48 @@ function timeout(duration) {
 const m = timeout(5000).then(() => {
     console.log("done!");
 });
+
+    
+/*------------------------PROMISES---------------2---------*/ 
+let P = new Promise((resolve, reject) => {
+    
+    //cleaning the room
+    let isClean = true;
+    
+    if (isClean){
+        resolve("CLEAN!");
+    } else {
+        reject("NOT CLEAN!");
+    }
+});
+    
+P.then(fromResolve => {
+    console.log(`The room is ${fromResolve}`);
+}).catch(fromReject => {
+    console.log(`The room is ${fromReject}`);
+});
+    
+//-> The room is CLEAN!
+    
+      
+/*------------------------PROMISES-----------------3-------*/ 
+let clean = (message) => new Promise((resolve, reject) => {
+  resolve("Cleaned the room.");  
+});
+    
+ 
+let win = (message) => new Promise((resolve, reject) => {
+  resolve(`${message} Won an IceCream.`);  
+});
+    
+ clean()
+     .then(res => win(res))
+     .then(res => {
+        console.log(`Finished. (${res})`);
+    });
+  
+//-> Finished. (Cleaned the room. Won an IceCream.)
+
 
 
 
